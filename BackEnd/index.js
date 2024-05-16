2b8b35217b4f43c832fa14c8ccb6849a110f5745
WaritTeerapong <warit.teer@kmutt.ac.th>
2024-05-16 18:30:32 +0700

    add cors in index.js, Done api logout

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
 const authRouter = require("./routes/auth.route.js");
 
 dotenv.config();
@@ -20,9 +22,16 @@ const app = express();
 const port = 5000;
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-app.use("/backend/auth", authRouter);
 app.use("/backend/auth", authRouter);
