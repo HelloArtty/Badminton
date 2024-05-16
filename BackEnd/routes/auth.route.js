2d286513ddcc61b5db5c259362683df308590377
WaritTeerapong <warit.teer@kmutt.ac.th>
2024-05-16 18:57:42 +0700

    add cors in index.js, Done api logout

--- BackEnd/routes/auth.route.js
+++ BackEnd/routes/auth.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const registerController = require("../controller/auth.register.js");
-const loginController = require("../controller/auth.login.js");
-const logoutController = require("../controller/auth.logout.js");
+const registerController = require("../controller/auth-register.js");
+const loginController = require("../controller/auth-login.js");
+const logoutController = require("../controller/auth-logout.js");
 
 const router = express.Router();
 
