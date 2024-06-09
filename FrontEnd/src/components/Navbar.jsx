9a9b7b50813a1910b6bcd4e8b672e15419abe7d3
WaritTeerapong <warit.teer@kmutt.ac.th>
2024-06-09 22:58:59 +0700

    change colour
    - navbar
    - booking court

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,64 +1,94 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Swal from 'sweetalert2';
-import { AxiosLib } from '../lib/axios';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import { AxiosLib } from "../lib/axios";
 
 const Navbar = () => {
-    const [user, setUser] = useState(null);
-    const [isMenuOpen, setIsMenuOpen] = useState(false); // State for controlling dropdown visibility
+  const [user, setUser] = useState(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State for controlling dropdown visibility
 
-    useEffect(() => {
-        const storedUser = JSON.parse(localStorage.getItem('user'));
-        setUser(storedUser);
-    }, []);
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    setUser(storedUser);
+  }, []);
 
-    const handleLogout = async () => {
-        try {
-            await AxiosLib.post('/backend/auth/log-out');
-            if (typeof window !== 'undefined') {
-                localStorage.removeItem('token');
-                localStorage.removeItem('user');
-                window.location.href = '/login';
-            }
-        } catch {
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Something went wrong. Please try again.',
-            });
-        }
-    };
+  const handleLogout = async () => {
+    try {
+      await AxiosLib.post("/backend/auth/log-out");
+      if (typeof window !== "undefined") {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        window.location.href = "/login";
+      }
+    } catch {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong. Please try again.",
+      });
+    }
+  };
 
-    return (
-        <nav className="bg-gray-800 p-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="flex items-center">
-                    <Link to="/booking" className="text-white font-bold py-2 px-4 rounded">Badminton Link</Link>
-                </div>
+  return (
+    <nav className="bg-emerald-600  p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex items-center">
+          <Link
+            to="/booking"
+            className="text-white font-bold py-2 px-4 rounded"
+          >
+            Badminton Link
+          </Link>
+        </div>
 
-                <div className="flex items-center">
-                    {user && (
-                        <>
-                            <span className="text-white mr-2">{user.username}</span>
-                            <div className="relative">
-                                <img onClick={() => setIsMenuOpen(!isMenuOpen)} src={user.img} className="w-12 h-12 rounded-full border-2 border-gray-300 cursor-pointer" alt="User Avatar" />
-                                {isMenuOpen && (
-                                    <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1">
-                                        <Link to="/profile" className="flex px-4 justify-center py-2 text-gray-800 hover:bg-gray-200">My Booking</Link>
-                                        <Link to="/booking" className="flex px-4 justify-center py-2 text-gray-800 hover:bg-gray-200">Booking</Link>
-                                        <button onClick={handleLogout} className="w-full block px-4 py-2 text-gray-800 hover:bg-gray-200">Log Out</button>
-                                    </div>
-                                )}
-                            </div>
-                        </>
-                    )}
-                    {!user && (
-                        <Link to="/login" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Log In</Link>
-                    )}
-                </div>
-            </div>
-        </nav>
-    );
+        <div className="flex items-center">
+          {user && (
+            <>
+              <span className="text-white mr-2">{user.username}</span>
+              <div className="relative">
+                <img
+                  onClick={() => setIsMenuOpen(!isMenuOpen)}
+                  src={user.img}
+                  className="w-12 h-12 rounded-full border-2 border-gray-300 cursor-pointer"
+                  alt="User Avatar"
+                />
+                {isMenuOpen && (
+                  <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1">
+                    <Link
+                      to="/profile"
+                      className="flex px-4 justify-center py-2 text-gray-800 hover:bg-gray-200"
+                    >
+                      My Booking
+                    </Link>
+                    <Link
+                      to="/booking"
+                      className="flex px-4 justify-center py-2 text-gray-800 hover:bg-gray-200"
+                    >
+                      Booking
+                    </Link>
+                    <button
+                      onClick={handleLogout}
+                      className="w-full block px-4 py-2 text-gray-800 hover:bg-gray-200"
+                    >
+                      Log Out
+                    </button>
+                  </div>
+                )}
+              </div>
+            </>
+          )}
+          {!user && (
+            <Link
+              to="/login"
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Log In
+            </Link>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- FrontEnd/src/pages/Booking.jsx
+++ FrontEnd/src/pages/Booking.jsx
@@ -1,92 +1,99 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 function Booking() {
-    const [countdown, setCountdown] = useState(0);
-    const [showNavbar, setShowNavbar] = useState(true);
-    const [countdownComplete, setCountdownComplete] = useState(false);
-    const [nextCountdownDate, setNextCountdownDate] = useState(null);
+  const [countdown, setCountdown] = useState(0);
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [countdownComplete, setCountdownComplete] = useState(false);
+  const [nextCountdownDate, setNextCountdownDate] = useState(null);
 
-    useEffect(() => {
-        if (countdown <= 0) {
-            setCountdownComplete(true);
-        }
-    }, [countdown]);
+  useEffect(() => {
+    if (countdown <= 0) {
+      setCountdownComplete(true);
+    }
+  }, [countdown]);
 
-    useEffect(() => {
-        const setNextCountdown = () => {
-            let now = new Date();
-            let countDownDate13 = new Date();
-            countDownDate13.setHours(13, 0, 0, 0);
+  useEffect(() => {
+    const setNextCountdown = () => {
+      let now = new Date();
+      let countDownDate13 = new Date();
+      countDownDate13.setHours(13, 0, 0, 0);
 
-            let countDownDate20 = new Date();
-            countDownDate20.setHours(23, 0, 0, 0);
+      let countDownDate20 = new Date();
+      countDownDate20.setHours(23, 0, 0, 0);
 
-            if (now >= countDownDate20) {
-                countDownDate13.setDate(countDownDate13.getDate() + 1);
-                setNextCountdownDate(countDownDate13.getTime());
-            } else if (now >= countDownDate13) {
-                setNextCountdownDate(countDownDate13.getTime());
-            } else {
-                setNextCountdownDate(countDownDate13.getTime());
-            }
-        };
-
-        setNextCountdown();
-
-        const timer = setInterval(() => {
-            const now = new Date().getTime();
-            const distance = nextCountdownDate - now;
-            setCountdown(distance);
-
-            if (distance <= 0) {
-                setNextCountdown();
-            }
-        }, 1000);
-
-        return () => clearInterval(timer);
-    }, [nextCountdownDate]);
-
-    const formatTime = (time) => {
-        const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((time % (1000 * 60)) / 1000);
-
-        return `${hours} hours ${minutes} minutes ${seconds} seconds`;
+      if (now >= countDownDate20) {
+        countDownDate13.setDate(countDownDate13.getDate() + 1);
+        setNextCountdownDate(countDownDate13.getTime());
+      } else if (now >= countDownDate13) {
+        setNextCountdownDate(countDownDate13.getTime());
+      } else {
+        setNextCountdownDate(countDownDate13.getTime());
+      }
     };
 
-    return (
-        <>
-            {showNavbar && <Navbar />}
-            <div className="min-h-screen bg-emerald-200 flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold mt-4 mb-6">Booking</h1>
-                {countdown > 0 ? (
-                    <>
-                        <h2 className="text-4xl mb-4">Waiting...</h2>
-                        <p>It's not yet time to book.</p>
-                        <p>{formatTime(countdown)}</p>
-                    </>
-                ) : (
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mb-5">
-                        {['Court 1', 'Court 2', 'Court 3', 'Court 4'].map((court, index) => (
-                            <Link to={`/court/${index + 1}`} key={index}>
-                                <div className="bg-emerald-100 rounded-l p-6">
-                                    <h2 className="text-2xl mb-4 font-semibold text-center">{court}</h2>
-                                    <div className="h-84 bg-emerald-100 flex items-center justify-center rounded-lg mb-4">
-                                        <img className="h-[450px] w-[300px] object-cover "
-                                            src="../src/assets/badminton.jpg" alt="Court" />
-                                    </div>
-                                </div>
-                            </Link>
-                        ))}
+    setNextCountdown();
+
+    const timer = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = nextCountdownDate - now;
+      setCountdown(distance);
+
+      if (distance <= 0) {
+        setNextCountdown();
+      }
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [nextCountdownDate]);
+
+  const formatTime = (time) => {
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((time % (1000 * 60)) / 1000);
+
+    return `${hours} hours ${minutes} minutes ${seconds} seconds`;
+  };
+
+  return (
+    <>
+      {showNavbar && <Navbar />}
+      <div className="min-h-screen  flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold  mt-4 mb-6">Booking</h1>
+        {countdown > 0 ? (
+          <>
+            <h2 className="text-4xl mb-4">Waiting...</h2>
+            <p>It's not yet time to book.</p>
+            <p>{formatTime(countdown)}</p>
+          </>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 mb-5">
+            {["Court 1", "Court 2", "Court 3", "Court 4"].map(
+              (court, index) => (
+                <Link to={`/court/${index + 1}`} key={index}>
+                  <div className="bg-lime-200 rounded-l p-6">
+                    <h2 className="text-2xl mb-4 font-semibold text-center">
+                      {court}
+                    </h2>
+                    <div className="h-84 bg-emerald-100 flex items-center justify-center rounded-lg mb-4">
+                      <img
+                        className="h-[450px] w-[300px] object-cover "
+                        src="../src/assets/badminton.jpg"
+                        alt="Court"
+                      />
                     </div>
-                )}
-            </div>
-            <Footer />
-        </>
-    );
+                  </div>
+                </Link>
+              )
+            )}
+          </div>
+        )}
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Booking;
