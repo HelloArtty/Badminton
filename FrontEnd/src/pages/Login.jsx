a37a9e6767f40960c3e8d846dae2649f82a2f817
WaritTeerapong <warit.teer@kmutt.ac.th>
2024-06-10 11:03:04 +0700

    Edit Login page and Regsiter page describtion

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -1,116 +1,109 @@
-import { TextField } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Swal from 'sweetalert2';
-import { AxiosLib } from '../lib/axios';
-
-
+import { TextField } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import { AxiosLib } from "../lib/axios";
 
 function Login() {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
 
-    useEffect(() => {
-        document.body.style.overflow = 'hidden';
-        return () => {
-            document.body.style.overflow = 'auto';
-        };
-    }, []);
+  const [login, setLogin] = useState({
+    email: "",
+    password: "",
+  });
 
+  const handleChange = (e) => {
+    setLogin({ ...login, [e.target.name]: e.target.value });
+  };
 
-    const [login, setLogin] = useState({
-        email: '',
-        password: '',
-    })
-
-    const handleChange = (e) => {
-        setLogin({ ...login, [e.target.name]: e.target.value })
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const result = await AxiosLib.post("/backend/auth/log-in", {
+        email: login.email,
+        password: login.password,
+      });
+      if (result.status === 200)
+        localStorage.setItem("user", JSON.stringify(result.data.user));
+      window.location.href = "/booking";
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Email or Password is incorrect",
+      });
     }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-            const result = await AxiosLib.post('/backend/auth/log-in', { email: login.email, password: login.password })
-            if (result.status === 200)
-                localStorage.setItem('user', JSON.stringify(result.data.user));
-            window.location.href = '/booking'
-        } catch (error) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Email or Password is incorrect',
-            })
-        }
-    }
-
-
-    return (
-        <>
-            <div className="items-center justify-center flex min-h-screen w-full h-full">
-                <div className="bg-emerald-600 min-h-screen w-1/2 p-8 hidden md:block">
-                    <div className="p-8 min-h-screen items-center justify-center flex">
-                        <div className="max-w-md p-8 text-center">
-                            <h2 className="text-2xl text-gray-100 mb-2">
-                                Welcome back to
-                            </h2>
-                            <h1 className="text-8xl text-white font-bold mb-3">
-                                BadLink
-                            </h1>
-                            <h3 className="text-sm text-gray-200  mb-2">
-                                We're thrilled to see you again. Your return means a lot to us, and we're excited to continue our journey together.
-                                Dive back in and explore what's new since your last visit.
-                            </h3>
-
-                        </div>
-                    </div>
-                </div>
-                <div className="bg-white w-full md:w-1/2 p-8 min-h-screen flex items-center justify-center ">
-                    <div className="max-w-md bg-white p-8 w-full flex flex-col justify-center">
-                        <h2 className="text-2xl font-bold mb-2">
-                            Login
-                        </h2>
-                        <h3 className="text-sm text-gray-600 mb-4">
-                            Welcome! Please fill email and password to login into your account.
-                        </h3>
-                        <form onSubmit={handleSubmit}>
-                            {/* Email */}
-                            <div className="mb-4">
-                                <TextField
-                                    className="w-full px-3 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-emerald-600 focus:border-transparent"
-                                    label="Email"
-                                    type="email"
-                                    id="email"
-                                    name="email"
-                                    placeholder="Enter your email"
-                                    onChange={handleChange}
-                                // required
-                                />
-                            </div>
-                            {/* Password */}
-                            <div className="mb-4">
-                                <TextField
-                                    className='w-full px-3 py-2 text-sm border rounded-lg shadow-sm'
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    name="password"
-                                    onChange={handleChange}
-                                // required
-                                />
-                            </div>
-                            <button className="w-full bg-emerald-600 text-white py-2 rounded-lg hover:bg-emerald-700">
-                                Login
-                            </button>
-                        </form>
-                        <p className="mt-4 text-center text-sm">
-                            Don’t have an account? &nbsp;
-                            <Link className="text-emerald-600 hover:underline" to="/register">
-                                Create an account
-                            </Link>
-                        </p>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="items-center justify-center flex min-h-screen w-full h-full">
+        <div className="bg-emerald-600 min-h-screen w-1/2 p-8 hidden md:block">
+          <div className="p-8 min-h-screen items-center justify-center flex">
+            <div className="max-w-md p-8 text-center">
+              <h2 className="text-2xl text-gray-100 mb-2">Welcome back to</h2>
+              <h1 className="text-8xl text-white font-bold mb-3">BadLink</h1>
+              <h3 className="text-sm text-gray-200  mb-2">
+                Welcome back again, we are here to help you. Log in now to book
+                your badminton court and continue your winning streak. Let's
+                make every game count! together.
+              </h3>
             </div>
-        </>
-    );
+          </div>
+        </div>
+        <div className="bg-white w-full md:w-1/2 p-8 min-h-screen flex items-center justify-center ">
+          <div className="max-w-md bg-white p-8 w-full flex flex-col justify-center">
+            <h2 className="text-2xl font-bold mb-2">Login</h2>
+            <h3 className="text-sm text-gray-600 mb-4">
+              Welcome! Please fill email and password to login into your
+              account.
+            </h3>
+            <form onSubmit={handleSubmit}>
+              {/* Email */}
+              <div className="mb-4">
+                <TextField
+                  className="w-full px-3 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-emerald-600 focus:border-transparent"
+                  label="Email"
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  onChange={handleChange}
+                  // required
+                />
+              </div>
+              {/* Password */}
+              <div className="mb-4">
+                <TextField
+                  className="w-full px-3 py-2 text-sm border rounded-lg shadow-sm"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  name="password"
+                  onChange={handleChange}
+                  // required
+                />
+              </div>
+              <button className="w-full bg-emerald-600 text-white py-2 rounded-lg hover:bg-emerald-700">
+                Login
+              </button>
+            </form>
+            <p className="mt-4 text-center text-sm">
+              Don’t have an account? &nbsp;
+              <Link className="text-emerald-600 hover:underline" to="/register">
+                Create an account
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
--- FrontEnd/src/pages/Register.jsx
+++ FrontEnd/src/pages/Register.jsx
@@ -1,170 +1,204 @@
-import { TextField } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import Swal from 'sweetalert2';
-import { AxiosLib } from '../lib/axios';
+import { TextField } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import { AxiosLib } from "../lib/axios";
 
 function Register() {
-    useEffect(() => {
-        document.body.style.overflow = 'hidden';
-        return () => {
-            document.body.style.overflow = 'auto';
-        };
-    }, []);
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
 
-    const [register, setRegister] = useState({
-        username: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
+  const [register, setRegister] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    setRegister({
+      ...register,
+      [e.target.name]: e.target.value,
     });
+    console.log(register);
+  };
 
-    const handleChange = (e) => {
-        setRegister({
-            ...register,
-            [e.target.name]: e.target.value
-        });
-        console.log(register);
-    };
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      if (
+        !register.username ||
+        !register.email ||
+        !register.password ||
+        !register.confirmPassword
+      ) {
+        return Swal.fire("Error", "Please fill all the fields", "error");
+      } else if (register.username.length < 6) {
+        return Swal.fire(
+          "Error",
+          "Username must be at least 6 characters",
+          "error"
+        );
+      } else if (register.username.includes(" ")) {
+        return Swal.fire("Error", "Username cannot contain space", "error");
+      } else if (!register.email.includes("@")) {
+        return Swal.fire("Error", "Please fill email correctly", "error");
+      } else if (register.password.length < 8) {
+        return Swal.fire(
+          "Error",
+          "Password must be at least 8 characters",
+          "error"
+        );
+      } else if (!register.password.match(/[0-9]/g)) {
+        return Swal.fire(
+          "Error",
+          "Password must contain at least one number",
+          "error"
+        );
+      } else if (!register.password.match(/[A-Z]/g)) {
+        return Swal.fire(
+          "Error",
+          "Password must contain at least one uppercase",
+          "error"
+        );
+      } else if (!register.password.match(/[a-z]/g)) {
+        return Swal.fire(
+          "Error",
+          "Password must contain at least one lowercase",
+          "error"
+        );
+      } else if (!register.password.match(/[^\w\s]/g)) {
+        return Swal.fire(
+          "Error",
+          "Password must contain at least one special character",
+          "error"
+        );
+      } else if (register.password.includes(" ")) {
+        return Swal.fire("Error", "Password cannot contain space", "error");
+      } else if (register.password !== register.confirmPassword) {
+        return Swal.fire(
+          "Error",
+          "Password must match the confirm password",
+          "error"
+        );
+      }
+      const createNewUser = {
+        username: register.username,
+        email: register.email,
+        password: register.password,
+      };
+      const result = await AxiosLib.post(
+        "/backend/auth/register",
+        createNewUser
+      );
+      console.log(result);
+      if (result.status === 200) {
+        return (window.location.href = "/login");
+      }
+    } catch (error) {
+      if (
+        error.response.status === 400 ||
+        error.response.status === 500 ||
+        error.response.status === 409
+      ) {
+        return Swal.fire("Error", error.response.data.message, "error");
+      }
+    }
+    console.log(register);
+  };
 
-    const handleSubmit = async (e) => {
-        try {
-            e.preventDefault();
-            if (!register.username || !register.email || !register.password || !register.confirmPassword) {
-                return Swal.fire('Error', 'Please fill all the fields', 'error');
-            } else if (register.username.length < 6) {
-                return Swal.fire('Error', 'Username must be at least 6 characters', 'error');
-            } else if (register.username.includes(' ')) {
-                return Swal.fire('Error', 'Username cannot contain space', 'error');
-            } else if (!register.email.includes('@')) {
-                return Swal.fire('Error', 'Please fill email correctly', 'error');
-            } else if (register.password.length < 8) {
-                return Swal.fire('Error', 'Password must be at least 8 characters', 'error');
-            } else if (!register.password.match(/[0-9]/g)) {
-                return Swal.fire('Error', 'Password must contain at least one number', 'error');
-            } else if (!register.password.match(/[A-Z]/g)) {
-                return Swal.fire('Error', 'Password must contain at least one uppercase', 'error');
-            } else if (!register.password.match(/[a-z]/g)) {
-                return Swal.fire('Error', 'Password must contain at least one lowercase', 'error');
-            } else if (!register.password.match(/[^\w\s]/g)) {
-                return Swal.fire('Error', 'Password must contain at least one special character', 'error');
-            } else if (register.password.includes(' ')) {
-                return Swal.fire('Error', 'Password cannot contain space', 'error');
-            } else if (register.password !== register.confirmPassword) {
-                return Swal.fire('Error', 'Password must match the confirm password', 'error');
-            }
-            const createNewUser = {
-                username: register.username,
-                email: register.email,
-                password: register.password,
-            };
-            const result = await AxiosLib.post('/backend/auth/register', createNewUser);
-            console.log(result);
-            if (result.status === 200) {
-                return (window.location.href = '/login');
-            }
-        } catch (error) {
-            if (error.response.status === 400 || error.response.status === 500 || error.response.status === 409) {
-                return Swal.fire('Error', error.response.data.message, 'error');
-            }
-        }
-        console.log(register);
-    };
-
-    return (
-        <>
-            <div className="items-center justify-center flex min-h-screen w-full h-full">
-                <div className="bg-emerald-600 min-h-screen w-1/2 p-8 hidden md:block">
-                    <div className="p-8 min-h-screen items-center justify-center flex">
-                        <div className="max-w-md p-8 text-center">
-                            <h2 className="text-2xl text-gray-100 mb-2">
-                                Welcome to
-                            </h2>
-                            <h1 className="text-8xl text-white font-bold mb-3">
-                                BadLink
-                            </h1>
-                            <h3 className="text-sm text-gray-200  mb-2">
-                                We're excited to have you join us. By registering,
-                                you're taking the first step towards an enriching experience filled with opportunities to connect, learn, and grow.
-                            </h3>
-
-                        </div>
-                    </div>
-                </div>
-                <div className="bg-white w-full md:w-1/2 p-8 min-h-screen flex items-center justify-center ">
-                    <div className="max-w-md bg-white p-8 w-full flex flex-col justify-center">
-                        <h2 className="text-2xl font-bold mb-2">
-                            Create an account
-                        </h2>
-                        <h3 className="text-sm text-gray-600 mb-4">
-                        Welcome! Please fill in your username, email, and password to register your account. 
-                        </h3>
-                        <form onSubmit={handleSubmit}>
-                            {/* Username */}
-                            <div className="mb-4">
-                                <TextField
-                                    className="w-full px-3 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-emerald-600 focus:border-transparent"
-                                    label="Username"
-                                    type="text"
-                                    id="username"
-                                    name="username"
-                                    placeholder="Enter your username"
-                                    onChange={handleChange}
-                                // required
-                                />
-                            </div>
-                            {/* Email */}
-                            <div className="mb-4">
-                                <TextField
-                                    className="w-full px-3 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-emerald-600 focus:border-transparent"
-                                    label="Email"
-                                    type="email"
-                                    id="email"
-                                    name="email"
-                                    placeholder="Enter your email"
-                                    onChange={handleChange}
-                                // required
-                                />
-                            </div>
-                            {/* Password */}
-                            <div className="mb-4">
-                                <TextField
-                                    className='w-full px-3 py-2 text-sm border rounded-lg shadow-sm'
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    name="password"
-                                    onChange={handleChange}
-                                // required
-                                />
-                            </div>
-                            {/* Confirm Password */}
-                            <div className="mb-4">
-                                <TextField
-                                    className='w-full px-3 py-2 text-sm border rounded-lg'
-                                    label="Confirm Password"
-                                    type="password"
-                                    id="confirmPassword"
-                                    name="confirmPassword"
-                                    onChange={handleChange}
-                                // required
-                                />
-                            </div>
-                            <button className="w-full bg-emerald-600 text-white py-2 rounded-lg hover:bg-emerald-700">
-                                Create Account
-                            </button>
-                        </form>
-                        <p className="mt-4 text-center text-sm">
-                            Already have an account? &nbsp;
-                            <Link className="text-emerald-600 hover:underline" to="/login">
-                                Login
-                            </Link>
-                        </p>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="items-center justify-center flex min-h-screen w-full h-full">
+        <div className="bg-emerald-600 min-h-screen w-1/2 p-8 hidden md:block">
+          <div className="p-8 min-h-screen items-center justify-center flex">
+            <div className="max-w-md p-8 text-center">
+              <h2 className="text-2xl text-gray-100 mb-2">Welcome to</h2>
+              <h1 className="text-8xl text-white font-bold mb-3">BadLink</h1>
+              <h3 className="text-sm text-gray-200  mb-2">
+                Register now to secure your court, stay active, and have fun.
+                Let's smash those fitness goals together!
+              </h3>
             </div>
-        </>
-    );
+          </div>
+        </div>
+        <div className="bg-white w-full md:w-1/2 p-8 min-h-screen flex items-center justify-center ">
+          <div className="max-w-md bg-white p-8 w-full flex flex-col justify-center">
+            <h2 className="text-2xl font-bold mb-2">Create an account</h2>
+            <h3 className="text-sm text-gray-600 mb-4">
+              Welcome! Please fill in your username, email, and password to
+              register your account.
+            </h3>
+            <form onSubmit={handleSubmit}>
+              {/* Username */}
+              <div className="mb-4">
+                <TextField
+                  className="w-full px-3 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-emerald-600 focus:border-transparent"
+                  label="Username"
+                  type="text"
+                  id="username"
+                  name="username"
+                  placeholder="Enter your username"
+                  onChange={handleChange}
+                  // required
+                />
+              </div>
+              {/* Email */}
+              <div className="mb-4">
+                <TextField
+                  className="w-full px-3 py-2 text-sm border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-emerald-600 focus:border-transparent"
+                  label="Email"
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  onChange={handleChange}
+                  // required
+                />
+              </div>
+              {/* Password */}
+              <div className="mb-4">
+                <TextField
+                  className="w-full px-3 py-2 text-sm border rounded-lg shadow-sm"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  name="password"
+                  onChange={handleChange}
+                  // required
+                />
+              </div>
+              {/* Confirm Password */}
+              <div className="mb-4">
+                <TextField
+                  className="w-full px-3 py-2 text-sm border rounded-lg"
+                  label="Confirm Password"
+                  type="password"
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  onChange={handleChange}
+                  // required
+                />
+              </div>
+              <button className="w-full bg-emerald-600 text-white py-2 rounded-lg hover:bg-emerald-700">
+                Create Account
+              </button>
+            </form>
+            <p className="mt-4 text-center text-sm">
+              Already have an account? &nbsp;
+              <Link className="text-emerald-600 hover:underline" to="/login">
+                Login
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Register;
