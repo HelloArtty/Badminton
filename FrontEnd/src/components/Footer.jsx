ce7c3a1fe8e90284ce3e3f38e84fa80eef6fa032
WaritTeerapong <warit.teer@kmutt.ac.th>
2024-06-10 11:05:49 +0700

    Update Footer.jsx

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    const year = new Date().getFullYear();
+  const year = new Date().getFullYear();
 
-    return <footer className="flex justify-center items-center h-12">
-        <p>
-        {`Copyright © Upbeat Code ${year}`}
-        </p>
-        </footer>;
+  return (
+    <footer className="flex justify-center items-center h-12">
+      <p>{`© ${year} Badminton Link | CSS 234 Web Programming II`}</p>
+    </footer>
+  );
 };
 
 export default Footer;
